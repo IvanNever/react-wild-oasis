ec9d5ac3ba76e7a4cb359bc68b082e6c3a25e809
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-11 18:20:32 +0300

    added supabase connection

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,21 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins.js";
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      {/*<Heading as="h1">All cabins</Heading>*/}
+      {/*<p>TEST</p>*/}
+      <img
+        src="https://xbrxzggohzbmkrrjhbfb.supabase.co/storage/v1/object/public/cabin-images/cabin-002.jpg?t=2024-10-11T15%3A17%3A18.775Z"
+        alt="cabin"
+      />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://xbrxzggohzbmkrrjhbfb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhicnh6Z2dvaHpibWtycmpoYmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2NTQzNTQsImV4cCI6MjA0NDIzMDM1NH0.zZJ1NBkbAjknOS2UE4YuEoOaZBRLew6Dmhtp9vC-Ed4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
