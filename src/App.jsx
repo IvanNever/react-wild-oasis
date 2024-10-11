6e89cc0223320205fb2815e72e65130f3823bc57
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-11 16:37:25 +0300

    app layout, routing and main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Settings from "./pages/Settings.jsx";
+import Users from "./pages/Users.jsx";
+import Login from "./pages/Login.jsx";
+import Account from "./pages/Account.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import GlobalStyles from "./styles/GlobalStyle.js";
+import AppLayout from "./ui/AppLayout.jsx";
+
 export default function App() {
-  return <div>The initial state</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+            <Route path="check-in/:id" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
