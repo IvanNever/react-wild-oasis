23366a48fea0de18a1d777de413a68dd0be77500
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-14 13:22:47 +0300

    form errors handling and download image

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://xbrxzggohzbmkrrjhbfb.supabase.co";
+export const supabaseUrl = "https://xbrxzggohzbmkrrjhbfb.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhicnh6Z2dvaHpibWtycmpoYmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2NTQzNTQsImV4cCI6MjA0NDIzMDM1NH0.zZJ1NBkbAjknOS2UE4YuEoOaZBRLew6Dmhtp9vC-Ed4";
 const supabase = createClient(supabaseUrl, supabaseKey);
