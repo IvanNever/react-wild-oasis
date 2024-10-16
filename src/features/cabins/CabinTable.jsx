9d4fe3707bcdd674dde6aa0c95fb85b7934472d1
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-16 13:46:37 +0300

    filtering and sorting cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import CabinRow from "./CabinRow.jsx";
 import { useCabins } from "./useCabins.js";
 import Table from "../../ui/Table.jsx";
 import Menus from "../../ui/Menus.jsx";
+import { useSearchParams } from "react-router-dom";
 
 const TableHeader = styled.header`
   display: grid;
@@ -22,24 +23,62 @@ const TableHeader = styled.header`
 
 export default function CabinTable() {
   const { cabins, isPending } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isPending) return <Spinner />;
-  return (
-    <Menus>
-      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-        <Table.Header role="row">
-          <div></div>
-          <div>Cabin</div>
-          <div>Capacity</div>
-          <div>Price</div>
-          <div>Discount</div>
-          <div></div>
-        </Table.Header>
-        <Table.Body
-          data={cabins}
-          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-        />
-      </Table>
-    </Menus>
-  );
+
+  // Filter handling
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
+
+  if (filterValue === "all") {
+    filteredCabins = cabins;
+  }
+  if (filterValue === "no-discount") {
+    filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
+  }
+  if (filterValue === "with-discount") {
+    filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
+  }
+
+  // Sort by handling
+  const sortByValue = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortByValue.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  let sortedCabins;
+  if (field === "name") {
+    sortedCabins = filteredCabins.sort((a, b) => {
+      if (a["name"].toLowerCase() < b["name"].toLowerCase()) {
+        return -1 * modifier;
+      }
+      if (a["name"].toLowerCase() > b["name"].toLowerCase()) {
+        return 1 * modifier;
+      }
+      return 0;
+    });
+  } else {
+    sortedCabins = filteredCabins.sort(
+      (a, b) => (a[field] - b[field]) * modifier,
+    );
+  }
+
+  if (sortByValue)
+    return (
+      <Menus>
+        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+          <Table.Header role="row">
+            <div></div>
+            <div>Cabin</div>
+            <div>Capacity</div>
+            <div>Price</div>
+            <div>Discount</div>
+            <div></div>
+          </Table.Header>
+          <Table.Body
+            data={sortedCabins}
+            render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+          />
+        </Table>
+      </Menus>
+    );
 }
---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,22 @@
+import Select from "./Select.jsx";
+import { useSearchParams } from "react-router-dom";
+
+function SortBy({ options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || "";
+  function handleChange(value) {
+    searchParams.set("sortBy", value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type="white"
+      onChange={handleChange}
+    />
+  );
+}
+
+export default SortBy;
